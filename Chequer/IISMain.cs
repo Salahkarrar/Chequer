363df734c5fea835b4c6cbb906219b1cf1374e85
Salahkarrar <salah.karrar@icloud.com>
2024-12-26 02:33:55 +0300

    IIS Main Form Make Code Clean

--- Chequer/IISMain.cs
+++ Chequer/IISMain.cs
@@ -12,170 +12,76 @@ namespace Chequer
             ActiveForm.Text = "AAY | IIS | IIS Main";
             Dock = DockStyle.Fill;
         }
-        public int M { get; set; }
 
+        // Property to track permission status
+        public int conut { get; set; }
+
+        // Event handler for Cheque button click
         private void Cheque_Btn_Click(object sender, EventArgs e)
         {
-            Cheque myForm = new Cheque(null, null, null, null, this)
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Menu_Pan.Hide();
-                    Takaful_Pan.Hide();
-                    //Details_Pan.Controls.Clear();
-                    Details_Pan.Controls.Add(myForm);
-                    myForm.Dock = DockStyle.Fill;
-                    myForm.Show();
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            OpenForm(new Cheque(null, null, null, null, this));
         }
 
+        // Event handler for Contract button click
         private void Contract_Btn_Click(object sender, EventArgs e)
         {
-            Contracted myForm = new Contracted(this)
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Menu_Pan.Hide();
-                    Takaful_Pan.Hide();
-                    //Details_Pan.Controls.Clear();
-                    Details_Pan.Controls.Add(myForm);
-                    myForm.Dock = DockStyle.Fill;
-                    myForm.Show();
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            OpenForm(new Contracted(this));
         }
 
+        // Event handler for Archive button click
         private void Archive_Btn_Click(object sender, EventArgs e)
         {
-            Archive myForm = new Archive(this)
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Menu_Pan.Hide();
-                    Takaful_Pan.Hide();
-                    //Details_Pan.Controls.Clear();
-                    Details_Pan.Controls.Add(myForm);
-                    myForm.Dock = DockStyle.Fill;
-                    myForm.Show();
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            OpenForm(new Archive(this));
         }
 
+        // Event handler for Advanced Tax button click
         private void AdvancedTax_Btn_Click(object sender, EventArgs e)
         {
-            AdvancedTax myForm = new AdvancedTax(null, null, null, null, this)
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Menu_Pan.Hide();
-                    Takaful_Pan.Hide();
-                    //Details_Pan.Controls.Clear();
-                    Details_Pan.Controls.Add(myForm);
-                    myForm.Dock = DockStyle.Fill;
-                    myForm.Show();
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            OpenForm(new AdvancedTax(null, null, null, null, this));
         }
 
+        // Event handler for Takaful button click
         private void Takaful_Btn_Click(object sender, EventArgs e)
         {
-            Takaful myForm = new Takaful(null, null, null, null, this)
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Menu_Pan.Hide();
-                    Takaful_Pan.Hide();
-                    //Details_Pan.Controls.Clear();
-                    Details_Pan.Controls.Add(myForm);
-                    myForm.Dock = DockStyle.Fill;
-                    myForm.Show();
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            OpenForm(new Takaful(null, null, null, null, this));
         }
 
+        // Event handler for Other button click
         private void Other_Btn_Click(object sender, EventArgs e)
         {
-            TakafulAdd myForm = new TakafulAdd(null, null, null, null, this)
-            {
-                TopLevel = false
-            };
+            OpenForm(new TakafulAdd(null, null, null, null, this));
+        }
+
+        // Event handler for Takaful panel toggle button click
+        private void Takaful_Btn_Click_1(object sender, EventArgs e)
+        {
+            Takaful_Pan.Visible = !Takaful_Pan.Visible;
+        }
+
+        // Method to open a form and check permissions
+        private void OpenForm(Form myForm)
+        {
+            myForm.TopLevel = false;
             Login.Instance.Check(Login.Instance.UserName, myForm.Name);
+
             foreach (var obj in Login.Instance.List)
             {
                 if (myForm.Name == obj.FunctionName)
                 {
-                    M = 1;
+                    conut = 1;
                     Menu_Pan.Hide();
                     Takaful_Pan.Hide();
-                    //Details_Pan.Controls.Clear();
                     Details_Pan.Controls.Add(myForm);
                     myForm.Dock = DockStyle.Fill;
                     myForm.Show();
+                    return;
                 }
             }
-            if (M != 1)
+
+            if (conut != 1)
             {
                 MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
             }
-        }
-
-        private void Takaful_Btn_Click_1(object sender, EventArgs e)
-        {
-            if (Takaful_Pan.Visible == false)
-                Takaful_Pan.Show();
-            else
-                Takaful_Pan.Hide();
         }
     }
 }
