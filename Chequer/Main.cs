8b9c380cd1706601cfca12e1497f8329be25b67b
Salahkarrar <salah.karrar@icloud.com>
2024-12-26 02:30:17 +0300

    Main Form Improve code and make it clean

--- Chequer/Main.cs
+++ Chequer/Main.cs
@@ -6,117 +6,130 @@ using System.IO;
 using System.Windows.Forms;
 
 namespace Chequer
-
 {
     public partial class Main : Form
     {
+        // Database connection object
+
         public Main() => InitializeComponent();
 
-        public OleDbConnection Con { get; } = new OleDbConnection(Resources.Path);
+        // Color properties
+        public Color Tran => Color.Transparent;
+        public Color Blue => Color.FromArgb(59, 115, 185);
 
-        private void SetIsLogout(bool value)
-        {
-            IsLogout = value;
-        }
-
-        public Color Tran { get => Color.Transparent; }
-        public Color Blue { get => Color.FromArgb(59, 115, 185); }
+        // Public properties
         public bool IsLogout { get; set; }
-        public int M { get; set; }
+        public int count { get; set; }
 
+        // Property to set logout status
+        private void SetIsLogout(bool value) => IsLogout = value;
+
+        // Event handler for form closing
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (e.CloseReason == CloseReason.UserClosing)
             {
-                dynamic dialog = MessageBox.Show("Do You Really Want to Closing The Program?", "Exit", MessageBoxButtons.YesNo);
+                var dialog = MessageBox.Show("Do You Really Want to Close The Program?", "Exit", MessageBoxButtons.YesNo);
                 if (dialog == DialogResult.Yes)
                 {
-                    var backupFolder = Application.StartupPath + "\\Backup\\";
-
-                    var backupFileName = String.Format("{0}{1}~{2}.mdb",
-                        backupFolder, "Chequer",
-                        DateTime.Now.ToString("dd-MM-yyyy  hh-mm-ss"));
-
-                    string So = Application.StartupPath +"\\Database\\Chequer.mdb";
-                    string De = backupFileName;
-
-                    string backupDir = Resources.PathBackup;
-                    var DeletionDays = 2;
-                    if (DeletionDays < 10)
-                    {
-                        try
-                        {
-                            string[] files = Directory.GetFiles(backupDir);
-
-                            foreach (string file in files)
-                            {
-                                FileInfo fi = new FileInfo(file);
-                                if (fi.CreationTime < DateTime.Now.AddDays(-DeletionDays))
-                                {
-                                    fi.Delete();
-                                }
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                    }
-                    File.Copy(So, De);
+                    BackupDatabase();
                     Application.Exit();
                 }
-                else if (dialog == DialogResult.No)
+                else
                 {
                     e.Cancel = true;
                 }
             }
         }
 
+        // Method to backup the database
+        private void BackupDatabase()
+        {
+            // Define the backup folder and file name with current date and time
+            var backupFolder = Path.Combine(Application.StartupPath, "Backup");
+            var backupFileName = $"{backupFolder}\\Chequer~{DateTime.Now:dd-MM-yyyy  hh-mm-ss}.mdb";
+            var sourceFile = Path.Combine(Application.StartupPath, "Database\\Chequer.mdb");
+            var backupDir = Resources.PathBackup;
+            var deletionDays = 2;
+
+            // Delete old backups if deletionDays is less than 10
+            if (deletionDays < 10)
+            {
+                try
+                {
+                    foreach (var file in Directory.GetFiles(backupDir))
+                    {
+                        var fileInfo = new FileInfo(file);
+                        if (fileInfo.CreationTime < DateTime.Now.AddDays(-deletionDays))
+                        {
+                            fileInfo.Delete();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            // Copy the current database file to the backup location
+            File.Copy(sourceFile, backupFileName);
+        }
+
+
+        // Event handler for form load
         private void Main_Load(object sender, EventArgs e)
         {
             Login.Instance.CheckMain(Login.Instance.UserName);
             foreach (var obj in Login.Instance.List)
             {
                 Login.Instance.RoleName = obj.RoleName;
-                Text = Resources.AppName + " Home ";
-                if (Home_Btn.Name == obj.FunctionName)
-                {
-                    Home_Btn.Enabled = true;
-                    Home_Btn.Tag = obj.FunctionName;
-                }
-                else if (IIS_Btn.Name == obj.FunctionName)
-                {
-                    IIS_Btn.Enabled = true;
-                    HReception_Btn.Enabled = true;
-                    IIS_Btn.Tag = obj.FunctionName;
-                }
-                else if (Search_Btn.Name == obj.FunctionName)
-                {
-                    Search_Btn.Enabled = true;
-                    HSearch_Btn.Enabled = true;
-                    Search_Btn.Tag = obj.FunctionName;
-                }
-                else if (Report_Btn.Name == obj.FunctionName)
-                {
-                    Report_Btn.Enabled = true;
-                    HReport_Btn.Enabled = true;
-                    Report_Btn.Tag = obj.FunctionName;
-                }
-                else if (Settings_Btn.Name == obj.FunctionName)
-                {
-                    Settings_Btn.Enabled = true;
-                    HSettings_Btn.Enabled = true;
-                    Settings_Btn.Tag = obj.FunctionName;
-                }
+                Text = $"{Resources.AppName} Home";
+                EnableButton(obj.FunctionName);
             }
 
             Home_Btn.Click += Home_Btn_Click;
             HomeBtnEnter(sender, e);
 
-            UserName_Lab.Text = "User Name : " + Login.Instance.UserName;
-            label4.Text = "Role : " + Login.Instance.RoleName;
+            UserName_Lab.Text = $"User Name : {Login.Instance.UserName}";
+            label4.Text = $"Role : {Login.Instance.RoleName}";
         }
 
+        // Method to enable buttons based on user role
+        private void EnableButton(string functionName)
+        {
+            switch (functionName)
+            {
+                case nameof(Home_Btn):
+                    EnableButton(Home_Btn, functionName);
+                    break;
+                case nameof(IIS_Btn):
+                    EnableButton(IIS_Btn, functionName);
+                    HReception_Btn.Enabled = true;
+                    break;
+                case nameof(Search_Btn):
+                    EnableButton(Search_Btn, functionName);
+                    HSearch_Btn.Enabled = true;
+                    break;
+                case nameof(Report_Btn):
+                    EnableButton(Report_Btn, functionName);
+                    HReport_Btn.Enabled = true;
+                    break;
+                case nameof(Settings_Btn):
+                    EnableButton(Settings_Btn, functionName);
+                    HSettings_Btn.Enabled = true;
+                    break;
+            }
+        }
+
+        // Helper method to enable a button and set its tag
+        private void EnableButton(Button button, string functionName)
+        {
+            button.Enabled = true;
+            button.Tag = functionName;
+        }
+
+        // Event handler for sign out button click
         private void Singout_Btn_Click(object sender, EventArgs e)
         {
             SetIsLogout(true);
@@ -124,184 +137,117 @@ namespace Chequer
             Hide();
         }
 
-        /*Side Panel*/
-
-        //Home;
+        // Event handler for home button click
         public void Home_Btn_Click(object sender, EventArgs e)
         {
-            ActiveForm.Text = Resources.AppName + " " + $"{Login.Instance.UserName}  |  " + $"{Login.Instance.RoleName}";
-            Side_Pan.Height = Home_Btn.Height;
-            Side_Pan.Top = Home_Btn.Top;
+            ActiveForm.Text = $"{Resources.AppName} {Login.Instance.UserName} | {Login.Instance.RoleName}";
+            UpdateSidePanel(Home_Btn);
             Main_Pan.Controls.Clear();
             Main_Pan.Controls.Add(Sub_Pan);
         }
+
+        // Event handler for home button enter
         public void HomeBtnEnter(object sender, EventArgs e)
         {
             Home_Btn.Focus();
             Home_Btn.BackColor = Blue;
         }
 
+        // Event handler for home button leave
         public void HomeBtnLeave(object sender, EventArgs e) => Home_Btn.BackColor = Tran;
 
-        //Reception;
+        // Event handler for reception button click
         public void ReceptionBtnClick(object sender, EventArgs e)
         {
-            Side_Pan.Height = IIS_Btn.Height;
-            Side_Pan.Top = IIS_Btn.Top;
-            IISMain myForm = new IISMain()
-            {
-                TopLevel = false
-
-            };
-            {
-                Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-                foreach (var obj in Login.Instance.List)
-                {
-                    if (myForm.Name == obj.FunctionName)
-                    {
-                        M = 1;
-                        Main_Pan.Controls.Clear();
-                        Main_Pan.Controls.Add(myForm);
-                        myForm.Show();
-                        break;
-                    }
-                }
-                if (M != 1)
-                {
-                    MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-                }
-            }
+            UpdateSidePanel(IIS_Btn);
+            LoadForm(new IISMain());
         }
+
+        // Event handler for reception button enter
         public void Reception_Btn_Enter(object sender, EventArgs e)
         {
             IIS_Btn.Focus();
             IIS_Btn.BackColor = Blue;
         }
 
+        // Event handler for reception button leave
         public void Reception_Btn_Leave(object sender, EventArgs e) => IIS_Btn.BackColor = Tran;
 
-        //Search;
+        // Event handler for search button click
         public void Search_Btn_Click(object sender, EventArgs e)
         {
-            Side_Pan.Height = Search_Btn.Height;
-            Side_Pan.Top = Search_Btn.Top;
-
-            SearchMain myForm = new SearchMain
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Main_Pan.Controls.Clear();
-                    Main_Pan.Controls.Add(myForm);
-                    myForm.Show();
-                    break;
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            UpdateSidePanel(Search_Btn);
+            LoadForm(new SearchMain());
         }
+
+        // Event handler for search button enter
         public void Search_Btn_Enter(object sender, EventArgs e)
         {
             Search_Btn.Focus();
             Search_Btn.BackColor = Blue;
         }
+
+        // Event handler for search button leave
         public void Search_Btn_Leave(object sender, EventArgs e) => Search_Btn.BackColor = Tran;
 
-        //Report;
+        // Event handler for report button click
         public void Report_Btn_Click(object sender, EventArgs e)
         {
-            Side_Pan.Height = Report_Btn.Height;
-            Side_Pan.Top = Report_Btn.Top;
-            ReportMain myForm = new ReportMain
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Main_Pan.Controls.Clear();
-                    Main_Pan.Controls.Add(myForm);
-                    myForm.Show();
-                    break;
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            UpdateSidePanel(Report_Btn);
+            LoadForm(new ReportMain());
         }
+
+        // Event handler for report button enter
         public void Report_Btn_Enter(object sender, EventArgs e)
         {
             Report_Btn.Focus();
             Report_Btn.BackColor = Blue;
         }
+
+        // Event handler for report button leave
         public void Report_Btn_Leave(object sender, EventArgs e) => Report_Btn.BackColor = Tran;
 
-        //Settings;
+        // Event handler for settings button click
         public void Settings_Btn_Click(object sender, EventArgs e)
         {
-            Side_Pan.Height = Settings_Btn.Height;
-            Side_Pan.Top = Settings_Btn.Top;
-            SettingsMain myForm = new SettingsMain
-            {
-                TopLevel = false
-            };
-            Login.Instance.Check(Login.Instance.UserName, myForm.Name);
-            foreach (var obj in Login.Instance.List)
-            {
-                if (myForm.Name == obj.FunctionName)
-                {
-                    M = 1;
-                    Main_Pan.Controls.Clear();
-                    Main_Pan.Controls.Add(myForm);
-                    myForm.Show();
-                    break;
-                }
-            }
-            if (M != 1)
-            {
-                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
-            }
+            UpdateSidePanel(Settings_Btn);
+            LoadForm(new SettingsMain());
         }
+
+        // Event handler for settings button enter
         public void Settings_Btn_Enter(object sender, EventArgs e)
         {
             Settings_Btn.Focus();
             Settings_Btn.BackColor = Blue;
         }
+
+        // Event handler for settings button leave
         public void Settings_Btn_Leave(object sender, EventArgs e) => Settings_Btn.BackColor = Tran;
 
-        //About;
+        // Event handler for about button click
         public void About_Btn_Click(object sender, EventArgs e)
         {
-            Side_Pan.Height = About_Btn.Height;
-            Side_Pan.Top = About_Btn.Top;
-            using (About myForm = new About())
+            UpdateSidePanel(About_Btn);
+            using (var myForm = new About())
             {
                 myForm.ShowDialog();
             }
         }
+
+        // Event handler for about button enter
         public void About_Btn_Enter(object sender, EventArgs e)
         {
             About_Btn.Focus();
             About_Btn.BackColor = Blue;
         }
+
+        // Event handler for about button leave
         public void About_Btn_Leave(object sender, EventArgs e) => About_Btn.BackColor = Tran;
 
-        //Sigout;
-
+        // Event handler for copyright click
         private void CopyRight_Click(object sender, EventArgs e)
         {
-            if (Convert.ToInt32(Login.Instance.RoleID) == 1 || Convert.ToInt32(Login.Instance.RoleID) == 2)
+            if (Login.Instance.RoleID == "1" || Login.Instance.RoleID == "2")
             {
                 SetIsLogout(true);
                 Login.Instance.Show();
@@ -309,15 +255,38 @@ namespace Chequer
             }
         }
 
+        // Event handler for form size changed
         private void Main_SizeChanged(object sender, EventArgs e)
         {
-            if (Width <= 700)
+            Menu_Pan.Width = Width <= 700 ? 60 : 175;
+        }
+
+        // Method to update the side panel position and height
+        private void UpdateSidePanel(Button button)
+        {
+            Side_Pan.Height = button.Height;
+            Side_Pan.Top = button.Top;
+        }
+
+        // Method to load a form into the main panel
+        private void LoadForm(Form form)
+        {
+            form.TopLevel = false;
+            Login.Instance.Check(Login.Instance.UserName, form.Name);
+            foreach (var obj in Login.Instance.List)
             {
-                Menu_Pan.Width = 60;
+                if (form.Name == obj.FunctionName)
+                {
+                    count = 1;
+                    Main_Pan.Controls.Clear();
+                    Main_Pan.Controls.Add(form);
+                    form.Show();
+                    break;
+                }
             }
-            else
+            if (count != 1)
             {
-                Menu_Pan.Width = 175;
+                MessageBoxEx.Show("Please Check Your Permission.", Resources.M01, MessageBoxButtons.OK, MessageBoxIcon.Information, 1000);
             }
         }
     }
